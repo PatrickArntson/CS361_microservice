8efbcd818fe2c82d68154deb8c73c3bdcdf87085
PatrickArntson <arntsonp@oregonstate.edu>
2021-10-22 16:26:02 -0700

    registration and login work

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const Port = process.env.port || 4004;
 require('dotenv').config();
 const StockUser = require('./stockUser'); 
-app.use(express.json())
+app.use(express.json());
 
 // database config
 const uri = process.env.DATABASE_URL;
@@ -17,52 +17,100 @@ connectDB();
 
 
 
-app.get('/register', async (req, res) => {
-    try{
-        const users = await User.find();
-        res.status(200).send({
-            message: 'it worked',
-            users
-        })
-    } catch(err) {  
-        res.status(500).send({
-            message: 'error'
-        })
-    }
-})
+// app.get('/register', async (req, res) => {
+//     try{
+//         const users = await User.find();
+//         res.status(200).send({
+//             message: 'it worked',
+//             users
+//         })
+//     } catch(err) {  
+//         res.status(500).send({
+//             message: 'error'
+//         })
+//     }
+// })
 
 
 app.post('/register', async (req, res) => {
     // console.log(req.body);
     if (req.body.collection == 'StockUser'){
-        // add 'find' statement to see if user is already registered.
-        bcrypt.hash(req.body.password, 10, function(err, hashedPassword){
+        // 'find' statement to see if user is already registered.
+        try {
+            var findUser = await StockUser.find({ 'email' : req.body.email}).exec();
+        }   catch (err){
+            return 'error occured';
+        }
+        if (findUser.length > 0){
+            res.send({
+                Already_registered : true,
+                newly_registered: false
+            })
+            return;
+        } 
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
             if(err) {
-                console.log("got here")
                 res.send({
-                    error: err
+                    message: 'error'
                 })
             }
             let user = new StockUser ({
                 email: req.body.email,
                 password: hashedPassword
             }) 
-            user.save()
+            await user.save()
             .then(user => {
-                res.json({
-                    Registered: true
-                })
+                res.send({
+                    Already_registered : false,
+                    newly_registered: true
+                }) 
             })
             .catch(error => {
-                res.json({
+                res.send({
                     message: 'error'
                 })
             })
         }) 
+        return;
     }
-    
+    return;
 });
 
+
+app.post('/login', async (req, res) => {
+    if (req.body.collection == 'StockUser'){
+        try {
+            var findUser = await StockUser.find({ 'email' : req.body.email}).exec();
+        } catch (err) {
+            res.send({
+                message: 'error'
+            })
+        }
+        if (findUser.length == 1){
+            bcrypt.compare(req.body.password, findUser[0].password, function(err, result){
+                if (err){
+                    res.send({
+                        message: 'error'
+                    })
+                }
+                if (result){
+                    res.send({
+                        Successful_login: true
+                    })
+                } else {
+                    res.send({
+                        Successful_login: false
+                    })
+                }
+            });
+        } else {
+            res.send({
+                User_not_found: true
+            })
+        }
+    }
+    return;
+})
 
 
 
