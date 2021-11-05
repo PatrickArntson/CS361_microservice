16027426b82c5ff2fdeb053a12915df5f4cfbc2c
PatrickArntson <arntsonp@oregonstate.edu>
2021-11-05 00:46:56 -0700

    using mongodb direct insead of mongoose, added other user functionality

--- server.js
+++ server.js
@@ -1,103 +1,204 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
 const Port = process.env.port || 4004;
-require('dotenv').config();
-const StockUser = require('./stockUser'); 
+require('dotenv').config(); 
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-// database config
+
+// Set up and connect to db
 const uri = process.env.DATABASE_URL;
-const connectDB = async()=>{
-    await mongoose.connect(uri);
-    console.log('database is succesfully connected');
-}
-connectDB();
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('database is sucessfuly connected')
+});
+// Database Collections
+const patricksCollection = client.db("cs361_databases").collection("stockusers");
+const joshsCollection = client.db("cs361_databases").collection("Josh");
+const esthersCollection = client.db("cs361_databases").collection("Esther");
 
 
 
 app.post('/register', async (req, res) => {
-    // console.log(req.body);
-    if (req.body.collection == 'StockUser'){
-        // 'find' statement to see if user is already registered.
-        try {
-            var findUser = await StockUser.find({ 'email' : req.body.email}).exec();
-        }   catch (err){
-            return 'error occured';
-        }
-        if (findUser.length > 0){
-            res.send({
-                Already_registered : true,
-                newly_registered: false
-            })
-            return;
+    try {
+        // connect to the specified collection
+        if (req.body.collection == 'StockUser'){
+            var findUser = await patricksCollection.find({email: req.body['email']}).toArray();
         } 
-        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
-            if(err) {
-                res.send({
-                    message: 'error'
-                })
-            }
-            let user = new StockUser ({
-                email: req.body.email,
-                password: hashedPassword
-            }) 
-            await user.save()
-            .then(user => {
-                res.send({
-                    Already_registered : false,
-                    newly_registered: true
-                }) 
-            })
-            .catch(error => {
-                res.send({
-                    message: 'error'
-                })
-            })
-        }) 
+        else if (req.body.collection == 'Josh'){
+            var findUser = await joshsCollection.find({email: req.body['email']}).toArray();
+        } 
+        else if (req.body.collection == 'Esther'){
+            var findUser = await esthersCollection.find({email: req.body['email']}).toArray();
+        } 
+    }   catch (err){
+        res.send({
+            errorMessage: 'error'
+        })
+        return console.log(err);
+    }
+    if (!findUser){
+        res.send({
+            errorMessage: 'Collection Not Found'
+        })
         return;
     }
+    if (findUser.length > 0){
+        res.send({
+            alreadyRegistered : true,
+            newlyRegistered: false
+        })
+        return;
+    } 
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if(err) {
+            res.send({
+                errorMessage: 'error'
+            })
+            return console.log(err);
+        }
+        try {
+            if (req.body.collection == 'StockUser'){
+                await patricksCollection.insertOne({email: req.body['email'], password: hashedPassword})
+            } 
+            else if (req.body.collection == 'Josh'){
+                await joshsCollection.insertOne({email: req.body['email'], password: hashedPassword})
+            } 
+            else if (req.body.collection == 'Esther'){
+                await esthersCollection.insertOne({email: req.body['email'], password: hashedPassword})
+            } 
+            res.send({
+                alreadyRegistered : false,
+                newlyRegistered: true
+            })
+        } catch (err){
+            res.send({
+                errorMessage: 'error'
+            })
+            return console.log(err);
+        }
+    }) 
+    return;
+    // }
     return;
 });
 
 
 app.post('/login', async (req, res) => {
-    console.log(req.body);
     if (req.body.collection == 'StockUser'){
         try {
-            var findUser = await StockUser.find({ 'email' : req.body.email}).exec();
+            var findUser = await patricksCollection.find({email: req.body['email']}).toArray();
         } catch (err) {
             res.send({
-                message: 'error'
+                errorMessage: 'error'
             })
+            return console.log(err);
         }
         if (findUser.length == 1){
-            bcrypt.compare(req.body.password, findUser[0].password, function(err, result){
+            bcrypt.compare(req.body.password, findUser[0]['password'], function(err, result){
                 if (err){
                     res.json({
-                        message: 'error'
+                        errorMessage: 'error'
                     })
+                    return console.log(err);
                 }
-                console.log(result);
                 if (result){
                     res.json({
-                        User_not_found: false,
-                        Successful_login: true
+                        userNotFound: false,
+                        successfulLogin: true
                     })
                 } else {
                     res.json({
-                        User_not_found: false,
-                        Successful_login: false
+                        userNotFound: false,
+                        successfulLogin: false
                     })
                 }
             });
         } else {
             res.json({
-                User_not_found: true,
-                Successful_login: false
+                userNotFound: true,
+                successfulLogin: false
             })
         }
+    }
+    else if (req.body.collection == 'Josh'){
+        try {
+            var findUser = await joshsCollection.find({email: req.body['email']}).toArray();
+        } catch (err) {
+            res.send({
+                errorMessage: 'error'
+            })
+            return console.log(err);
+        }
+        if (findUser.length == 1){
+            bcrypt.compare(req.body.password, findUser[0]['password'], function(err, result){
+                if (err){
+                    res.json({
+                        errorMessage: 'error'
+                    })
+                    return console.log(err);
+                }
+                if (result){
+                    res.json({
+                        userNotFound: false,
+                        successfulLogin: true
+                    })
+                } else {
+                    res.json({
+                        userNotFound: false,
+                        successfulLogin: false
+                    })
+                }
+            });
+        } else {
+            res.json({
+                userNotFound: true,
+                successfulLogin: false
+            })
+        }
+    }
+    else if (req.body.collection == 'Esther'){
+        try {
+            var findUser = await esthersCollection.find({email: req.body['email']}).toArray();
+        } catch (err) {
+            res.send({
+                errorMessage: 'error'
+            })
+            return console.log(err);
+        }
+        if (findUser.length == 1){
+            bcrypt.compare(req.body.password, findUser[0]['password'], function(err, result){
+                if (err){
+                    res.json({
+                        errorMessage: 'error'
+                    })
+                    return console.log(err);
+                }
+                if (result){
+                    res.json({
+                        userNotFound: false,
+                        successfulLogin: true
+                    })
+                } else {
+                    res.json({
+                        userNotFound: false,
+                        successfulLogin: false
+                    })
+                }
+            });
+        } else {
+            res.json({
+                userNotFound: true,
+                successfulLogin: false
+            })
+        }
+    }
+    else {
+        res.send({
+            errorMessage: 'Collection Not Found'
+        })
     }
     return;
 })
